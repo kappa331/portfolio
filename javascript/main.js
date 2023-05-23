81afabb5bd1034b308a77fab8dfc6cbd8c6317a4
kappa <wawata1qwerty@gmail.com>
2023-05-23 19:01:28 +0900

    2023-05-23

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,26 @@
-console.log('test');
+function getAge(b_y, b_m, b_d){
+    var today = new Date();
+    var y = today.getFullYear();
+    var m = today.getMonth() + 1;
+    var d = today.getDate();
+    var age = y - b_y;
+    if(b_m > m || (b_m == m && b_d > d)){
+        age--;
+    }
+    return age;
+};
+
+function setAge(){
+    var elem = document.getElementById("age");
+    elem.innerText =  getAge(1990, 1, 10);
+};
+
+window.addEventListener('DOMContentLoaded', function(){
+    setAge();
+    const elems = document.getElementsByClassName('item');
+    for(var elem of elems){
+        elem.addEventListener('click', function(){
+            this.nextElementSibling.classList.toggle('active');
+        });
+    };
+});
